c69f2ec5e49619da631e06853b4e5909249b357a
Jeong-hansol <jhs486674@gmail.com>
2020-07-16 18:36:16 +0900

    상단 메뉴바 스크롤시 고정기능 추가 js

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,14 @@ $(window).scroll(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 800);  // 화면 스크롤 수치만큼 화살표 흐려짐(1-스크롤 된 만큼 / 800)
 
   });
+
+
+var bannerOffset = $('.menu').offset();
+  $(window).scroll( function() {  // window에 스크롤링이 발생하면
+    if($(document).scrollTop() > bannerOffset.top ) {   // 위치 및 사이즈를 파악하여 미리 정한 css class를 add 또는 remove 한다.
+      $('.menu').addClass('menuFixed');
+    }
+    else {
+      $('.menu').removeClass('menuFixed');
+    }
+  });
