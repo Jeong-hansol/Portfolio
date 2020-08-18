9c0a088f405d7a131b72cf20e04176442b560b28
Jeong-hansol <jhs486674@gmail.com>
2020-08-18 14:09:15 +0900

    햄버거메뉴 보이기 / 숨기기

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,12 @@ $(window).on('load', function () {
      $("#load").hide();
 }); // loading 되면 이미지 숨기기
 
+// Mobile Menu
+$("#menu-media").hide();
+$("#menu-btn").click(function(){
+$("#menu-media").slideToggle();
+});
+
 
 $(window).scroll(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 800);  // 화면 스크롤 수치만큼 화살표 흐려짐(1-스크롤 된 만큼 / 800)
